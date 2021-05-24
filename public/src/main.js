afcceec1f806d3afc81249ffbdc3d98af623ce10
pome-ta <pome-ta@users.noreply.github.com>
2021-05-24 17:04:15 +0900

    add roms

--- public/src/main.js
+++ public/src/main.js
@@ -4,8 +4,10 @@ import { NES } from './nes.js';
 
 
 // const ROMDATA = 'test01';
-const ROMDATA = 'giko013';
+//const ROMDATA = 'giko013';
+//const ROMDATA = 'giko017';
 // const ROMDATA = 'nestest';
+const ROMDATA = 'lj65';
 
 const nes_path = new URL(`static/roms/${ROMDATA}.nes`, location.protocol + '//' + location.host + location.pathname).href
 
